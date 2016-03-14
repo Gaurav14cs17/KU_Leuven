ecdd4e69ea6de3c48cf69ca8b211a4de1c602be3
David <torrejonmoya.david@gmail.com>
2016-03-14 12:45:14 +0100

    Error at creating last category

--- TBIR/analogies_finder.py
+++ TBIR/analogies_finder.py
@@ -4,7 +4,7 @@ import time
 
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
-
+from random import seed, uniform
 
 def cosine(a, b):
     """
@@ -111,11 +111,15 @@ def build_outputs_list(filename):
         v0.2 returns dict of lists depending on category
     """
     outputs = {}
+    expected = []
     category = ""
-    first_category = True
+    first_category = True #controls FIRST CATEGORY ONLY
+    category_output= []
+
     with open(filename) as fd_outputs:
         for output in fd_outputs:
             if output[0].isdigit():
+                #print output
                 #save in the dictionary
                 if first_category is False:
                     outputs[category] = category_output
@@ -126,7 +130,11 @@ def build_outputs_list(filename):
                 category_output = []
             else:
                 category_output.append(output)
-    return outputs
+                expected.append(output)
+
+    outputs[category] = category_output
+
+    return outputs, expected
 
 
 def find_analogies(input_file, ouptut_file):
@@ -142,55 +150,83 @@ def find_analogies(input_file, ouptut_file):
         gotta load glove to ram, for uses sake
         should add a counter for the a b c found in the glove model!!
     """
+    seed = 1337 #l33t
+
+    percentage_test = 0.1 #make this optional or maybe a parameter for the person to decide?
 
     glove_model = build_glove_dictionary() #returns obviously the glove dictionary
-    possible_outputs_dict = build_outputs_list(ouptut_file) #d'
+    possible_outputs_dict, expected_output = build_outputs_list(ouptut_file) #d'
+    #print possible_outputs_dict.keys()
     analogies_computed = 0
+
+    correct_mult = 0
+    correct_add = 0
+    correct_dir = 0
+
+    line_counter = 0 #this refers to the current output that we are looking for
 
     with open(input_file) as fd_inputs:  #
         for input in fd_inputs:
+            #print input, expected_output[line_counter]
+            test_accepted = False
             if input[0].isdigit() is False:
+
+                num = uniform(1.0, 0.0)
                 category_bool = False
-                split_input = input.split(" ")
-                a, b, c = split_input[0], split_input[1], split_input[2].rstrip('\n')
-                d = np.zeros(len(a))
-                max_d_mult = 0
-                max_d_add = 0
-                max_d_dir  = 0
-                output_mult = None
-                output_add = None
-                output_dir = None
-                print 'trying...', a, b, c
-                start = time.time()
-                if (a in glove_model) and (b in glove_model) and (c in glove_model):
-                    for d_poo in possible_outputs:
-                        d_po = d_poo.rstrip('\n')
-                        if d_po in glove_model:
-                            #print d_po, 'found...'
-                            analogies_computed += 1
-                            # pass another value depending on which type of similarity is applied
-                            d_current_mult, d_current_add, d_current_dir  = get_cosine_similarity(glove_model[a], glove_model[b], glove_model[c], glove_model[d_po])
-                            #so pretty
-                            if d_current_mult > max_d_mult:
-                                max_d_mult = d_current_mult
-                                output_mult = d_po
-                            if d_current_add > max_d_add:
-                                max_d_add = d_current_add
-                                output_add = d_po
-                            if d_current_dir > max_d_dir:
-                                max_d_dir = d_current_dir
-                                output_dir = d_po
-                        else:
-                            #make this a lil bit more efficient....
-                            possible_outputs.remove(d_poo)
+
+                if num<percentage_test:
+                    test_accepted = True
+                    split_input = input.split(" ")
+                    a, b, c = split_input[0], split_input[1], split_input[2].rstrip('\n')
+                    d = np.zeros(len(a))
+                    max_d_mult = 0
+                    max_d_add = 0
+                    max_d_dir  = 0
+                    output_mult = None
+                    output_add = None
+                    output_dir = None
+                    print 'trying...', a, b, c
+                    start = time.time()
+                    if (a in glove_model) and (b in glove_model) and (c in glove_model):
+                        for d_poo in possible_outputs:
+                            d_po = d_poo.rstrip('\n')
+                            if d_po in glove_model:
+                                #print d_po, 'found...'
+                                analogies_computed += 1
+                                # pass another value depending on which type of similarity is applied
+                                d_current_mult, d_current_add, d_current_dir  = get_cosine_similarity(glove_model[a], glove_model[b], glove_model[c], glove_model[d_po])
+                                #so pretty
+                                if d_current_mult > max_d_mult:
+                                    max_d_mult = d_current_mult
+                                    output_mult = d_po
+                                if d_current_add > max_d_add:
+                                    max_d_add = d_current_add
+                                    output_add = d_po
+                                if d_current_dir > max_d_dir:
+                                    max_d_dir = d_current_dir
+                                    output_dir = d_po
+                            else:
+                                #make this a lil bit more efficient....
+                                possible_outputs.remove(d_poo)
+                line_counter += 1
             else:
                 category_bool = True
                 possible_outputs = possible_outputs_dict[input]
 
-            if category_bool is False:
-                print 'output(mult) found: ', output_mult, 'took:', time.time()-start
-                print 'output(add) found: ', output_add, 'took:', time.time()-start
-                print 'output(dir) found: ', output_dir, 'took:', time.time()-start
+            if category_bool is False and test_accepted:
+                print 'output(mult) found: ', output_mult, 'expected: ', expected_output[line_counter-1]
+                print 'output(add) found: ', output_add, 'expected: ', expected_output[line_counter-1]
+                print 'output(dir) found: ', output_dir, 'expected: ', expected_output[line_counter-1]
+                if output_mult is expected_output[line_counter-1]:
+                    correct_mult +=1
+                if output_add is expected_output[line_counter-1]:
+                    correct_add +=1
+                if output_dir is expected_output[line_counter-1]:
+                    correct_dir +=1
 
-    print analogies_computed
+
+
+    print 'multi correct: ', correct_mult/analogies_computed
+    print 'add correct: ', correct_add/analogies_computed
+    print 'dir correct: ', correct_dir/analogies_computed
     #compute cosine similarity and argmax...
