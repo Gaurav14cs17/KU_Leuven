071aff6962238a88dbff65fc107693944398e584
David <torrejonmoya.david@gmail.com>
2016-05-02 17:28:11 +0200

    Update run.py

--- CV/project/run.py
+++ CV/project/run.py
@@ -1,1 +1,16 @@
 __author__ = 'david_torrejon & Bharath Venkatesh'
+
+
+"""
+Schedule:
+wed:
+  @david: read files and load it x = x + Pb
+  numpy array
+thurs:
+ @bharath: move pca coord into this xD PCA PCA
+"""
+
+def PCA():
+  print "PCA"*5
+  
+PCA()
