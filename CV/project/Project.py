7e5b7aa49e243bb8e444c39d2e2631045edc1891
Bharath Venkatesh <bharath.venkatesh85@gmail.com>
2016-05-06 20:18:01 +0200

    Removed crazy symbolic link, updated python version of Ipython notebook

--- CV/project/Project.py
+++ CV/project/Project.py
@@ -7,7 +7,7 @@
 
 # ## Jupyter Magics
 
-# In[57]:
+# In[94]:
 
 get_ipython().magic(u'matplotlib inline')
 import matplotlib.pyplot as plt
@@ -19,13 +19,19 @@ def show(img):
 
 # ## load_landmarks.py - Load Landmarks
 
-# In[58]:
+# In[95]:
 
 __author__ = 'david_torrejon & Bharath Venkatesh'
 import os
 import numpy as np
 import string
 
+def extractLandmarksForIncisorByIndex(landmarks,index):
+    shapes=[]
+    for key in landmarks.keys():
+        shapes.append(landmarks[key][index])
+    return shapes
+    
 def load_landmarks(path='./Project Data(2)/_Data/Landmarks/original/',mirrored=False):
     """
     x1
@@ -35,24 +41,28 @@ def load_landmarks(path='./Project Data(2)/_Data/Landmarks/original/',mirrored=F
     ...
     xn
     yn
-    returns a numpy matrix of shape (nb_docs, nb_points, 2)
+    returns a list of list of shapes (nb_docs, nb_points, 2)
     """
     all_=string.maketrans('','')
     nodigs=all_.translate(all_, string.digits)
     landmarks = {}
+    ordering = {}
 
     if os.path.isdir(path):
         for filename in os.listdir(path):
             filepath = path+filename
-            name = filename.split("-")[0]
-            key = name.translate(all_, nodigs)
+            parts=filename.split("-") 
+            key = parts[0].translate(all_, nodigs)
+            #Need to remember the file index as well
+            index=parts[1].translate(all_, nodigs)
             if mirrored:
                 key=str(int(key)-14)
             #print key
             if key not in landmarks.keys():
                 landmarks[key] = []
-
+                ordering[key]=[]
             #print key
+            ordering[key].append(index)
             with open(filepath) as fd:
                 landmarks_file = []
                 for i, line in enumerate(fd):
@@ -64,15 +74,21 @@ def load_landmarks(path='./Project Data(2)/_Data/Landmarks/original/',mirrored=F
 
                 tmp = landmarks[key]
                 #print landmarks
-                landmarks[key].append(np.asarray(landmarks_file))
+                landmarks[key].append(landmarks_file)
                 #print landmarks[key]
     #print landmarks.keys()
+    #Reordering each landmark list
+    for key in landmarks.keys():
+        #print ordering[key]
+        myorder=sorted(range(len(ordering[key])), key=lambda k: ordering[key][k])
+        #print myorder
+        landmarks[key] = [ landmarks[key][i] for i in myorder]
     return landmarks
 
 
 # ## load_images.py - Load Images
 
-# In[59]:
+# In[96]:
 
 __author__ = 'david_torrejon & Bharath Venkatesh'
 
@@ -111,29 +127,111 @@ def load_images(landmarks, path='./Project Data(2)/_Data/Radiographs/'):
 
 
 # ## gpa.py - Do generalized procrustes analyses
-# See Appendix A - Aligning the Training Set, pg 21 of
+# See Appendix D Appendix D Aligning Two Shapes and Appendix A - Aligning the Training Set, pg 21 of
 # Cootes, Tim, E. R. Baldock, and J. Graham. "An introduction to active shape models." 
 # Image processing and analysis (2000): 223-248.
 # 
 
-# In[60]:
+# In[97]:
 
-'''
-1. Translate each example so that its centre of gravity is at the origin.
-2. Choose one example as an initial estimate of the mean shape and scale so that |x| = sqrt(x1^2+x2^2..)=1 
-3. Record the first estimate as x0 to define the default orientation.
-4. Align all the shapes with the current estimate of the mean shape. 
-5. Re-estimate the mean from aligned shapes.
-6. Apply constraints on scale and orientation to the current estimate of the mean by aligning it with ¯
-   x0 and scaling so that |x| = 1.
-7. If not converged, return to 4.
-   (Convergence is declared if the estimate of the mean does not change significantly after an iteration)
-'''
+import math
+import numpy as np
+
+def plotShapes(shapes):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for points in shapes:
+        xlist,ylist=zip(*(points))
+        ax.plot(xlist,ylist)
+
+def normalizeShape(l):
+    '''
+    Normalizes the array of landmarks and returns the norm
+    '''
+    norm=np.linalg.norm(l)
+    return l/norm,norm
+
+def centerShape(l):
+    '''
+    Centers the array of landmarks by subtracting the mean and returns the mean
+    '''
+    mu=np.mean(l,axis=0)
+    return l-mu,mu
+
+def centerAndScaleShape(l):
+    '''
+    Centers the array of landmarks by subtracting the mean and returns the mean and
+    scales, returning the centered and scaled vector , the mean and the norm
+    '''
+    cl,mu=centerShape(l)
+    ncl,norm=normalizeShape(cl)
+    return ncl,mu,norm
+
+def alignAtoBProcrustes(A,B):
+    '''
+    Compute parameters to optimally align Two CENTERED (Mean 0) and SCALED (Unit norm) shapes A,B
+    Returns rotated and scaled matrix Rx1 that minimizes |x1R-x2| (least squares)
+    See Appendix D of 
+    Cootes, Tim, E. R. Baldock, and J. Graham. "An introduction to active shape models." 
+    Image processing and analysis (2000): 223-248.
+    
+    Derivation T- transpose I - inverse PI -pseudoinverse
+    x1*R=x2
+    T(x2)*x1*R=T(x2)*x2 
+    Let C = T(x2)*x1
+    R=I(C)T(x2)*x2
+    For least squares fit find pseudoinverse of I using svd write C=U*S*T(V) then
+    PI(C)=V*I(S)*T(U)
+    R=V*I(S)*T(U)
+
+    '''
+    C=np.dot(B.T,A)
+    BTB=np.dot(B.T,B)
+    U,s,V=np.linalg.svd(C)
+    Sinv=np.diag(np.reciprocal(s))
+    PIC=np.dot(np.dot(V.T,Sinv),U.T)
+    R=np.dot(PIC,BTB)
+    return np.dot(A,R),R 
+
+def gpa(shapes,tol=1e-5):
+    '''
+    Generalized Procustes Analysis, returns the rotated scales and shapes and the mean with all points rotated and scaled 
+    to be in the same coordinate system
+    See Appendix A of 
+    Cootes, Tim, E. R. Baldock, and J. Graham. "An introduction to active shape models." 
+    Image processing and analysis (2000): 223-248.
+      
+    1. Translate each example so that its centre of gravity is at the origin.
+    2. Choose one example as an initial estimate of the mean shape and scale so that |x| = sqrt(x1^2+x2^2..)=1 
+    3. Record the first estimate as x0 to define the default orientation.
+    4. Align all the shapes with the current estimate of the mean shape. 
+    5. Re-estimate the mean from aligned shapes.
+    6. Apply constraints on scale and orientation to the current estimate of the mean by aligning it with ¯
+       x0 and scaling so that |x| = 1.
+    7. If not converged, return to 4.
+       (Convergence is declared if the estimate of the mean does not change significantly after an iteration)
+    '''
+    X=np.array(shapes)
+    [N,l,u]=X.shape
+    for i in range(N):
+        X[i,:,:],_=centerShape(X[i,:,:])
+    [N,l,u]=X.shape
+    currmux0=X[0,:,:]
+    lastmux0 = currmux0
+    t=0
+    while(t==0 or np.linalg.norm(currmux0-lastmux0) > tol):
+        for i in range(N):
+            cshape,_=centerShape(X[i,:,:])
+            X[i,:,:],_=alignAtoBProcrustes(cshape,currmux0)
+        lastmux0=currmux0
+        currmux0,_,_=centerAndScaleShape(np.mean(X,axis=0))
+        t = t+1
+    return X.tolist(),currmux0,t
 
 
 # ## pca.py - Do principal component analysis
 
-# In[61]:
+# In[98]:
 
 import numpy as np
 import bisect
@@ -193,7 +291,7 @@ def pcaV(X,varianceFraction=0.9):
 
 # ## Reading Radiograms, Segmentation and Landmarks
 
-# In[62]:
+# In[99]:
 
 landmarks=load_landmarks(path='/home/bharath/workspace/CV/Project/data/Landmarks/original/')
 mirrored_landmarks=load_landmarks(path='/home/bharath/workspace/CV/Project/data/Landmarks/mirrored/',mirrored=True)
@@ -203,10 +301,27 @@ images,data=load_images(landmarks,path='/home/bharath/workspace/CV/Project/data/
 
 # ## Scratch
 
-# In[63]:
+# In[100]:
 
-#mirrored_landmarks['1']
-#landmarks['1']
 #show(images['1'])
-#show(mirrored_images['1'])
+#for index in range(8):
+#    shapes=extractLandmarksForIncisorByIndex(landmarks,index)
+#    centeredShapes = []
+#    for shape in shapes:
+#        cshape,mu=centerShape(shape)
+#        centeredShapes.append(cshape)
+#    plotShapes(centeredShapes)
+shapes=extractLandmarksForIncisorByIndex(landmarks,3)
+cshapes=[]
+for shape in shapes:
+    cshape,_=centerShape(shape)
+    cshapes.append(cshape)
+plotShapes(cshapes)
+ashape,_=alignAtoBProcrustes(cshapes[6],cshapes[1])
+plotShapes([cshapes[6],cshapes[1]])
+plotShapes([ashape,cshapes[1]])
+newshapes,meanShape,t=gpa(shapes)
+print t
+plotShapes([meanShape])
+plotShapes(newshapes)
 
